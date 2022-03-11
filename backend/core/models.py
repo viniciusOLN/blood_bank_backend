f4355b09d68549634a95501e3666b40c94887539
vinicius <vinicius853o@gmail.com>
2022-03-10 21:10:16 -0300

    initial models

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from cpf_field.models import CPFField
 
-# Create your models here.
+class User(AbstractUser):
+    name = models.Charfield(max_length = 250)
+    cpf = CPFField(max_length = 11)    
+
+class Donator(models.Model):
+    pass
+
+class Nurce(models.Model):
+    pass
+
+class Adress(models.Model):
+    pass
+
+class Phone(models.Model):
+    pass
+
+class Allergies(models.Model):
+    pass
+
+class Donation(models.Model):
+    pass
+
+class Tubes(models.Model):
+    pass
+
+class CollectionBags(models.Model):
+    pass
+
+class Exames(models.Model):
+    pass

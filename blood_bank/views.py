63984132509d624ffab988d77c9382bc7a6fb493
matheus costa <matheuscosta.tech@gmail.com>
2022-06-02 18:20:08 -0300

    Added function to create user

--- blood_bank/views.py
+++ blood_bank/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
 from blood_bank.serializers import UserSerializer
 from blood_bank.models import User
 from rest_framework.views import APIView
-from rest_framework.response import Response
 
-# Create your views here.
 
 class Signup(APIView):
-    def post(self, request):
-        return Response('hello world')
+    def post(self, request: Request):
+        self.register_new_user(request.data)
+        return Response({'msg': 'New user registered'})
+
+    def register_new_user(self, request_data):
+        serializer = UserSerializer(data=request_data)
+        if serializer.is_valid():
+            serializer.save()

1b1fa1503671b62d66fee7c83bda6fd25637f7fe
Marcos-Dantas <kisnaiver1223@gmail.com>
2022-09-12 21:27:02 -0300

    adicionado logout na api de autenticação

--- blood_bank/urls.py
+++ blood_bank/urls.py
@@ -6,4 +6,6 @@ app_name = 'blood_bank'
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
+    
 ]
--- blood_bank/views.py
+++ blood_bank/views.py
@@ -1,22 +1,14 @@
-from socket import create_server
-from django.shortcuts import render
 from blood_bank.serializers import SignupFormSerializer, LoginFormSerializer
 from blood_bank.models import MyUser
-from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
-from django.contrib.auth import login, authenticate
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.views import APIView
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
     """
      login no sistema usando email e password
@@ -28,14 +20,17 @@ def login(request):
             email = request.data.get('email')            
             user = MyUser.objects.filter(email=email).first()
   
-            token = Token.objects.get(user=user)
-            return Response({'token': token.key }) 
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'message': 'Usuário logado com sucesso.',
+                'token': token.key
+                }) 
         else:
             return Response(serializer._errors)
 
 
-import re
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def signup(request):
     """
     signup no sistema usando email e password
@@ -45,9 +40,24 @@ def signup(request):
 
         if serializer.is_valid():
             user = serializer.save()
-            token = Token.objects.create(user=user)
-            return Response({'token': token.key }) 
+            user.is_active = True
+            user.save()
+
+            token, created = Token.objects.get_or_create(user=user).key
+            return Response({
+                'message': 'Usuário registrado com sucesso.',
+                'token': token.key 
+                }) 
         else:
             return Response(serializer._errors)
 
-    
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    request.user.auth_token.delete()
+
+    response = {
+        'message': 'Usuário Deslogado com sucesso.'
+    }
+
+    return Response(response)
